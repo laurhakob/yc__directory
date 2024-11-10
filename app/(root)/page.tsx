e087c311d65f15de5fcf34f717f49ec9f7b5a906
laurhakob <laurhakob@gmail.com>
2024-11-10 12:37:52 +0400

    search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({searchParams}: {
+  searchParams: Promise<{query?: string}>
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container">
@@ -10,6 +15,7 @@ export default function Home() {
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
           Competitions.
         </p>
+        <SearchForm query={query} />
       </section>
     </>
   );
