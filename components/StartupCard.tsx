0554c62f62efa8a33849309c4f47b2471c515558
laurhakob <laurhakob@gmail.com>
2024-11-11 11:37:43 +0400

    sanity setup

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -47,8 +47,8 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
       </div>
       <Link href={`/startup/${_id}`}>
         <p className="startup-card_desc">{description}</p>
-        <img src={image} alt="placeholder" className="startup-card_img" />
-        {/* <Image src={image} alt="placeholder" className="startup-card_img" width={500} height={300} /> */}
+        {/* <img src={image} alt="placeholder" className="startup-card_img" /> */}
+        <Image src={image} alt="placeholder" className="startup-card_img" width={500} height={300} />
       </Link>
       <div className="flex-between gap-3 mt-5">
         <Link href={`/?query=${category.toLowerCase()}`}>
